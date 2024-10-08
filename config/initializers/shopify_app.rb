0cb930893c171595ad81fdeac8c5c2e85ed46535
Marco Arduini <mar.arduini@gmail.com>
2024-10-08 09:51:39 +0200

    Reformat config/initializers/shopify_app.rb

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,25 +1,25 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.old_secret = ""
-  config.scope = "read_products" # Consult this page for more scope options:
-                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
+  config.scope = "read_products,write_discounts" # Consult this page for more scope options:
+  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
-  config.new_embedded_auth_strategy = true
+  config.new_embedded_auth_strategy = false
 
   config.after_authenticate_job = false
   config.api_version = "2024-10"
-  config.shop_session_repository = 'Shop'
+  config.shop_session_repository = "Shop"
   config.log_level = :info
   config.reauth_on_access_scope_changes = true
   config.webhooks = [
-    { topic: "app/uninstalled", address: "webhooks/app_uninstalled"},
+    { topic: "app/uninstalled", address: "webhooks/app_uninstalled" },
     { topic: "customers/data_request", address: "webhooks/customers_data_request" },
-    { topic: "customers/redact", address: "webhooks/customers_redact"},
-    { topic: "shop/redact", address: "webhooks/shop_redact"}
+    { topic: "customers/redact", address: "webhooks/customers_redact" },
+    { topic: "shop/redact", address: "webhooks/shop_redact" }
   ]
 
-  config.api_key = ENV.fetch('SHOPIFY_API_KEY', '').presence
-  config.secret = ENV.fetch('SHOPIFY_API_SECRET', '').presence
+  config.api_key = ENV.fetch("SHOPIFY_API_KEY", "").presence
+  config.secret = ENV.fetch("SHOPIFY_API_SECRET", "").presence
 
   # You may want to charge merchants for using your app. Setting the billing configuration will cause the Authenticated
   # controller concern to check that the session is for a merchant that has an active one-time payment or subscription.
@@ -37,8 +37,8 @@ ShopifyApp.configure do |config|
   # )
 
   if defined? Rails::Server
-    raise('Missing SHOPIFY_API_KEY. See https://github.com/Shopify/shopify_app#requirements') unless config.api_key
-    raise('Missing SHOPIFY_API_SECRET. See https://github.com/Shopify/shopify_app#requirements') unless config.secret
+    raise("Missing SHOPIFY_API_KEY. See https://github.com/Shopify/shopify_app#requirements") unless config.api_key
+    raise("Missing SHOPIFY_API_SECRET. See https://github.com/Shopify/shopify_app#requirements") unless config.secret
   end
 end
 
@@ -48,13 +48,13 @@ Rails.application.config.after_initialize do
       api_key: ShopifyApp.configuration.api_key,
       api_secret_key: ShopifyApp.configuration.secret,
       api_version: ShopifyApp.configuration.api_version,
-      host: ENV['HOST'],
+      host: ENV["HOST"],
       scope: ShopifyApp.configuration.scope,
-      is_private: !ENV.fetch('SHOPIFY_APP_PRIVATE_SHOP', '').empty?,
+      is_private: !ENV.fetch("SHOPIFY_APP_PRIVATE_SHOP", "").empty?,
       is_embedded: ShopifyApp.configuration.embedded_app,
       log_level: :info,
       logger: Rails.logger,
-      private_shop: ENV.fetch('SHOPIFY_APP_PRIVATE_SHOP', nil),
+      private_shop: ENV.fetch("SHOPIFY_APP_PRIVATE_SHOP", nil),
       user_agent_prefix: "ShopifyApp/#{ShopifyApp::VERSION}"
     )
 
